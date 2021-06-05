250bf47f2d5d9d381690f064862fac9317eac11c
shahzaibansari123 <ansarishahzaib567@gmail.com>
2021-06-06 00:33:19 +0500

    work complete

---
+++ app.js
@@ -0,0 +1,39 @@
+var list= document.getElementById('list')
+
+function add(){
+    var input = document.getElementById('input')
+    var li= document.createElement('li')
+    var liText= document.createTextNode(input.value)
+    li.appendChild(liText)
+
+    var createedt = document.createElement('i');
+    createedt.setAttribute("class","fa fa-pencil-square-o");
+    createedt.setAttribute("aria-hidden","true");
+    li.appendChild(createedt);
+    createedt.setAttribute("onclick","edt(this)")
+    
+    var createdlt = document.createElement('i');
+    createdlt.setAttribute("class","fa fa-trash");
+    createdlt.setAttribute("aria-hidden","true");
+    li.appendChild(createdlt);
+    createdlt.setAttribute('onclick','dlt(this)')
+
+    list.appendChild(li)
+    input.value=""
+    var h= document.createElement('hr')
+    list.appendChild(h)
+}
+
+function dlt(e){
+    e.parentNode.remove()
+}
+
+function dltall(){
+    list.innerHTML = ""
+}
+
+function edt(e){
+   var edit=e.parentNode.firstChild.nodeValue
+   var editValue = prompt("Edit",edit)
+   e.parentNode.firstChild.nodeValue = editValue
+}
